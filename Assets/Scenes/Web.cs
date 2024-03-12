4ca264d85ecbdc266e8007aaa30efcb6c0150015
Nathan-Macabanti <nmacandcheese@gmail.com>
2024-03-12 20:50:30 +0800

    reading from database

--- Assets/Scenes/Web.cs
+++ Assets/Scenes/Web.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Web : MonoBehaviour
+public static class Web
 {
-    void Start()
+    public static Dictionary<string, CharacterData> character_data { get; private set; } = new();
+    public static string note { get; private set; }
+    public static IEnumerator GetAllCharacterData()
     {
-        // A correct website page.
-        StartCoroutine(GetRequest("http://localhost/MP/GetDate.php"));
-    }
-
-    IEnumerator GetRequest(string uri)
-    {
+        string uri = "http://localhost/MP/GetAllCharacters.php";
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
             // Request and wait for the desired page.
@@ -34,6 +32,55 @@ public class Web : MonoBehaviour
                     Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
                     break;
             }
+
+            string[] rows = webRequest.downloadHandler.text.Split("\n");
+            for (int i = 0; i < rows.Length - 1; i++)
+            {
+                string[] columns = rows[i].Split(",");
+                CharacterData cData = new(columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]);
+                character_data.Add(cData.frame, cData);
+            }
         }
     }
+
+    public static IEnumerator GetCharacterDescription(string frame)
+    {
+        string uri = "http://localhost/MP/GetCharacterDescription.php";
+        WWWForm form = new();
+        form.AddField("frame", frame);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(uri, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                note = www.downloadHandler.text;
+            }
+        }
+    }
+}
+
+public struct CharacterData
+{
+    public CharacterData(string id, string name, string frame, string rank, string element, string cClass)
+    {
+        this.id = id;
+        this.name = name;
+        this.frame = frame;
+        this.rank = rank;
+        this.element = element;
+        this.cClass = cClass;
+    }
+
+    public string id { get; private set; }
+    public string name { get; private set; }
+    public string frame { get; private set; }
+    public string rank { get; private set; }
+    public string element { get; private set; }
+    public string cClass { get; private set; }
 }
